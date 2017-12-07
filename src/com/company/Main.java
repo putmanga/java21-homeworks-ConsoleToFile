e030823ec26711bb8061cc1ce85b821d37241e23
Edward Kats <edward.kats@outlook.com>
2017-12-07 23:24:31 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Main {
     public static AtomicInteger counter = new AtomicInteger(0);
     public static String path;
+    //scanner would be enough
     private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
@@ -13,8 +14,12 @@ public class Main {
         getPathAndName();
 
         System.out.println("Введите строки:");
+        
+        //you could assign a default value instead of starting the business logic outside of the "while" scope
         String s = reader.readLine();
         while (!s.equals("-1")) {
+            //if "s" will get a default value - you'll be able to start reading before the thread and 
+            //pass the read result to the thread
             new FileCreator(s).start();
             s = reader.readLine();
         }
